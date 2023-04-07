eeeb840cd0244c17f50f62d18146dace23906822
Nicolas <mikola11forever@gmail.com>
2023-04-07 20:03:24 +0300

    Added boot complete receiver

---
+++ app/src/main/java/com/nicolas/aura/receiver/BootCompletedBroadcastReceiver.kt
@@ -0,0 +1,12 @@
+package com.nicolas.aura.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootCompletedBroadcastReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
